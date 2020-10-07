8cb488118bde91258834ae4b938112e314ca7ea2
christian Hill <christiansean1990@gmail.com>
2020-10-07 11:41:54 -0400

    listed arrays varaible declaration and character length requirment

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUppercase;
+var confirmLowercase;
 
+function generatePassword() {
+    var confirmLength = (prompt("How many characters would you like this password to contian?"));
+  }
+
+while(confirmLength <= 7 || confirmLength >=129) {
+    (alert("Password length must be between 8-128 characters Try again"));
+    var confirmLength = (prompt("How many characters would you like your password to contain?"));
+  }
+    
+    alert('Your password will have ${confirmLength} characters');
 
 
 
