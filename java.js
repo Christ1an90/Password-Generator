88b5affe8e6d31a7bba7babbcbde52725e649e95
christian Hill <christiansean1990@gmail.com>
2020-10-11 17:36:03 -0400

    password parameters

--- java.js
+++ java.js
@@ -3,7 +3,7 @@
 
 document.querySelector("#generate").addEventListener("click", writePassword);
 
-//  Arrays 
+
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -29,7 +29,6 @@ function generatePassword() {
      
       alert(`Your password will have ${confirmLength} characters`);
 
-   
     var confirmSpecialCharacter = confirm("Press OKAY to confirm if you would like to include special characters");
     var confirmNumericCharacter = confirm("Press OKAY to confirm if you would like to include numeric characters");    
     var confirmLowerCase = confirm("Press OKAY to confirm if you would like to include lowercase characters");
@@ -42,42 +41,3 @@ function generatePassword() {
         var confirmLowerCase = confirm("Press OKAY to confirm if you would like to include lowercase characters");
         var confirmUpperCase = confirm("Press OKAY to confirm if you would like to include uppercase characters");   
     } 
-
-      
-      var passwordCharacters = []
-      
-    if (confirmSpecialCharacter) {
-      passwordCharacters = passwordCharacters.concat(specialChar)
-    }
-
-    if (confirmNumericCharacter) {
-      passwordCharacters = passwordCharacters.concat(number)
-    }
-      
-    if (confirmLowerCase) {
-      passwordCharacters = passwordCharacters.concat(alphaLower)
-    }
-
-    if (confirmUpperCase) {
-      passwordCharacters = passwordCharacters.concat(alphaUpper)
-    }
-
-      console.log(passwordCharacters)
-
-      
-      var randomPassword = ""
-      
-      for (var i = 0; i < confirmLength; i++) {
-        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-        console.log(randomPassword)
-      }
-      return randomPassword;
-}
-
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
